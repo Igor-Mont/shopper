7ee194da4002794ca1a6c3e289146d8ae1145c14
Igor-Mont <igornathandev@gmail.com>
2024-08-28 10:15:11 -0300

    test: ensure MeasurementByImageController return 400 ('INVALID_DATA') if an invalid basse64 is provided

--- src/presentation/measurement-by-image.spec.ts
+++ src/presentation/measurement-by-image.spec.ts
@@ -1,10 +1,30 @@
+import { InvalidParamError } from './errors/invalid-param-error';
 import { MissingParamError } from './errors/missing-param-error';
 import { MeasurementByImageController } from './measurement-by-image';
+import { Base64Validator } from './protocols/base64-validor';
 
-const makeSut = (): MeasurementByImageController => new MeasurementByImageController();
+interface SutTypes {
+  sut: MeasurementByImageController;
+  base64ValidatorStub: Base64Validator;
+}
+
+const makeSut = (): SutTypes => {
+  class Base64ValidatorStub implements Base64Validator {
+    isValid(base64: string): boolean {
+      return true;
+    }
+  }
+
+  const base64ValidatorStub = new Base64ValidatorStub();
+  const sut = new MeasurementByImageController(base64ValidatorStub);
+  return {
+    sut,
+    base64ValidatorStub,
+  };
+};
 describe('MeasurementByImage Controller', () => {
   test('Should return error_code "INVALID_DATA" if no image is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httRequest = {
       body: {
         customer_code: 'any customer_code',
@@ -18,7 +38,7 @@ describe('MeasurementByImage Controller', () => {
   });
 
   test('Should return error_code "INVALID_DATA" if no customer_code is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httRequest = {
       body: {
         image: 'any base64',
@@ -32,7 +52,7 @@ describe('MeasurementByImage Controller', () => {
   });
 
   test('Should return error_code "INVALID_DATA" if no measure_datetime is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httRequest = {
       body: {
         image: 'any base64',
@@ -46,7 +66,7 @@ describe('MeasurementByImage Controller', () => {
   });
 
   test('Should return error_code "INVALID_DATA" if no measure_type is provided', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httRequest = {
       body: {
         image: 'any base64',
@@ -57,5 +77,21 @@ describe('MeasurementByImage Controller', () => {
     const httpResponse = sut.handle(httRequest);
     expect(httpResponse.error_code).toBe('INVALID_DATA');
     expect(httpResponse.error_description).toEqual(new MissingParamError('measure_type'));
+  });
+
+  test('Should return error_code "INVALID_DATA" if an invalid base64 is provided', () => {
+    const { sut, base64ValidatorStub } = makeSut();
+    jest.spyOn(base64ValidatorStub, 'isValid').mockReturnValueOnce(false);
+    const httRequest = {
+      body: {
+        image: 'invalid base64',
+        customer_code: 'any customer_code',
+        measure_datetime: 'any datetime',
+        measure_type: 'any',
+      },
+    };
+    const httpResponse = sut.handle(httRequest);
+    expect(httpResponse.error_code).toBe('INVALID_DATA');
+    expect(httpResponse.error_description).toEqual(new InvalidParamError('image'));
   });
 });
