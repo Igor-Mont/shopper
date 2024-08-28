f4b3507731e7e0d7a7d06ec9df200238210cd1e3
Igor-Mont <igornathandev@gmail.com>
2024-08-28 09:04:21 -0300

    feat: ensure MeasurementByImageController return an error if no customer_code is provided

--- src/presentation/measurementByImage.ts
+++ src/presentation/measurementByImage.ts
@@ -1,8 +1,17 @@
 export class MeasurementByImageController {
   handle(httpRequest: any): any {
-    return {
-      error_code: 400,
-      error_description: new Error('Missing param: image'),
-    };
+    if (!httpRequest.body.image) {
+      return {
+        error_code: 400,
+        error_description: new Error('Missing param: image'),
+      };
+    }
+
+    if (!httpRequest.body.customer_code) {
+      return {
+        error_code: 400,
+        error_description: new Error('Missing param: customer_code'),
+      };
+    }
   }
 }
