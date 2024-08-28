e4f74e1128daa39a158d34d24a58f529e6c94cac
Igor-Mont <igornathandev@gmail.com>
2024-08-28 10:58:45 -0300

    feat: ensure MeasurementByImageController return 500 'SERVER_ERROR' if Base64Validator throws

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,8 +1,16 @@
+import { ServerError } from '../errors/server-error';
 import { HttpResponseError } from '../protocols/http';
 
 export const invalidDataRequest = (error: Error): HttpResponseError => {
   return {
     error_code: 'INVALID_DATA',
     error_description: error,
+  };
+};
+
+export const serverError = (): HttpResponseError => {
+  return {
+    error_code: 'SERVER_ERROR',
+    error_description: new ServerError(),
   };
 };
--- src/presentation/measurement-by-image.ts
+++ src/presentation/measurement-by-image.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError } from './errors/invalid-param-error';
 import { MissingParamError } from './errors/missing-param-error';
-import { invalidDataRequest } from './helpers/http-helper';
+import { invalidDataRequest, serverError } from './helpers/http-helper';
 import { Base64Validator } from './protocols/base64-validor';
 import { Controller } from './protocols/controller';
 import { HttpRequest, HttpResponse } from './protocols/http';
@@ -9,17 +9,20 @@ export class MeasurementByImageController implements Controller {
   constructor(private readonly base64Validator: Base64Validator) {}
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['image', 'customer_code', 'measure_datetime', 'measure_type'];
+    try {
+      const requiredFields = ['image', 'customer_code', 'measure_datetime', 'measure_type'];
 
-    const { body: requestBody } = httpRequest;
-    for (const field of requiredFields) {
-      if (!requestBody[field]) {
-        return invalidDataRequest(new MissingParamError(field));
+      const { body: requestBody } = httpRequest;
+      for (const field of requiredFields) {
+        if (!requestBody[field]) {
+          return invalidDataRequest(new MissingParamError(field));
+        }
       }
+
+      const isBase64Valid = this.base64Validator.isValid(requestBody.image);
+      if (!isBase64Valid) return invalidDataRequest(new InvalidParamError('image'));
+    } catch (error) {
+      return serverError();
     }
-
-    const isBase64Valid = this.base64Validator.isValid(requestBody.image);
-
-    if (!isBase64Valid) return invalidDataRequest(new InvalidParamError('image'));
   }
 }
