f94e0f6f910aac47d74f60756d5ef206d424b41c
Igor-Mont <igornathandev@gmail.com>
2024-08-28 08:57:12 -0300

    test: ensure MeasurementByImageController returns 400 if no image is provided

---
+++ src/presentation/measurementByImage.spec.ts
@@ -0,0 +1,16 @@
+import { MeasurementByImageController } from './measurementByImage';
+
+describe('MeasurementByImage Controller', () => {
+  test('Should return 400 if no image is provided', () => {
+    const sut = new MeasurementByImageController();
+    const httRequest = {
+      body: {
+        customer_code: 'any customer_code',
+        measure_datetime: 'any datetime',
+        measure_type: 'any measure_type',
+      },
+    };
+    const httpResponse = sut.handle(httRequest);
+    expect(httpResponse.error_code).toBe(400);
+  });
+});
