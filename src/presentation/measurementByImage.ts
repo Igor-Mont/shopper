6fc8b581f5cd192a4c7e350cb7775671d80a26ff
Igor-Mont <igornathandev@gmail.com>
2024-08-28 09:00:57 -0300

    feat: ensure MeasurementByImageController return an error if no image is provided

--- src/presentation/measurementByImage.ts
+++ src/presentation/measurementByImage.ts
@@ -2,6 +2,7 @@ export class MeasurementByImageController {
   handle(httpRequest: any): any {
     return {
       error_code: 400,
+      error_description: new Error('Missing param: image'),
     };
   }
 }
