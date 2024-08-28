0996e72998d02447c3e5fe07fbfdc3ef84388ab7
Igor-Mont <igornathandev@gmail.com>
2024-08-28 09:37:47 -0300

    refactor: make MeasurementByImageController conforms to a protocol

--- src/presentation/measurementByImage.ts
+++ src/presentation/measurementByImage.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from './errors/missing-param-error';
 import { invalidDataRequest } from './helpers/http-helper';
+import { Controller } from './protocols/controller';
 import { HttpRequest, HttpResponse } from './protocols/http';
 
-export class MeasurementByImageController {
+export class MeasurementByImageController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['image', 'customer_code', 'measure_datetime', 'measure_type'];
 
