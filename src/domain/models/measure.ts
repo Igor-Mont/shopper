a350c359df54f32c321f54c787b2c58e7eb94af8
Igor-Mont <igornathandev@gmail.com>
2024-08-30 02:41:12 -0300

    feat: implement ConfirmMeasurement repository interfaces in infra layer

---
+++ src/domain/models/measure.ts
@@ -0,0 +1,7 @@
+export interface Measure {
+  measure_uuid: string;
+  measure_datetime: string;
+  measure_type: string;
+  has_confirmed: boolean;
+  image_url: string;
+}
--- src/infra/db/mongodb/measure-repository/measure.ts
+++ src/infra/db/mongodb/measure-repository/measure.ts
@@ -8,10 +8,19 @@ import {
   ChecksReadingInMonthDTO,
   ChecksReadingInMonthRepository,
 } from '../../../../data/protocols/checks-reading-in-month';
+import { ConfirmMeasurementRepository } from '../../../../data/protocols/confirm-measurement-repository';
+import { FindMeasurementsRepository } from '../../../../data/protocols/find-measurements-repository';
+import { Measure } from '../../../../domain/models/measure';
 import { MeasureByImageModel } from '../../../../domain/models/measure-by-image';
 import { mongoHelper } from '../helpers/mongo-helper';
 
-export class MeasureRepository implements AddMeasureByImageRepository, ChecksReadingInMonthRepository {
+export class MeasureRepository
+  implements
+    AddMeasureByImageRepository,
+    ChecksReadingInMonthRepository,
+    ConfirmMeasurementRepository,
+    FindMeasurementsRepository
+{
   async add(addMeasureByImageDTO: AddMeasureByImageDTO): Promise<MeasureByImageModel> {
     const measurementsCollection = mongoHelper.getCollection('measurements');
     const { insertedId: id } = await measurementsCollection.insertOne({
@@ -42,5 +51,29 @@ export class MeasureRepository implements AddMeasureByImageRepository, ChecksRea
     }).length;
 
     return measuresInMonth === 0;
+  }
+
+  async findById(measure_uuid: string): Promise<Measure | null> {
+    const measurementsCollection = mongoHelper.getCollection('measurements');
+
+    const result = await measurementsCollection.findOne({ measure_uuid });
+
+    if (!result) return null;
+
+    const { image_url, measure_type, measure_datetime, has_confirmed } = mongoHelper.map(result);
+
+    return {
+      measure_uuid,
+      has_confirmed,
+      image_url,
+      measure_datetime,
+      measure_type,
+    };
+  }
+
+  async confirm(measure_uuid: string, measure_value: number): Promise<void> {
+    const measurementsCollection = mongoHelper.getCollection('measurements');
+
+    await measurementsCollection.updateOne({ measure_uuid }, { $set: { measure_value, has_confirmed: true } });
   }
 }
